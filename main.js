e45e5f7cad06709bd8e1fd34d1ac1aaca421b892
Wijayaac <wijayaac25@gmail.com>
2024-01-29 17:01:45 +0800

    Update : footer & header

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-function initVideoPlyr() {
-  const videoElm = document.querySelector("#player");
-  if (videoElm) {
-    const videoPlayer = new Plyr("#player", {
-      hideControls: true,
-    });
-  }
+function initVideoPlyr () {
+	const videoElm = document.querySelector("#player")
+	if (videoElm) {
+		const videoPlayer = new Plyr("#player", {
+			hideControls: true,
+		})
+	}
 }
 
-initVideoPlyr();
+initVideoPlyr()
+
+
+function headerNavigation () {
+	const headerElm = document.querySelector('.header')
+	if (!headerElm) {
+		return
+	}
+
+	const headerNav = document.querySelector('nav[data-visible]')
+	// toggle menu
+	const headerToggle = document.querySelector('.header__toggle')
+	headerToggle.addEventListener('click', function () {
+		let isExpanded = headerToggle.getAttribute('aria-expanded')
+		if (isExpanded === 'false') {
+			headerToggle.setAttribute('aria-expanded', 'true')
+			headerNav.setAttribute('data-visible', 'true')
+
+		} else {
+			headerToggle.setAttribute('aria-expanded', 'false')
+			headerNav.setAttribute('data-visible', 'false')
+		}
+
+	})
+}
+
+headerNavigation()
