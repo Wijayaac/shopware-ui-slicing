895be29dc37d624eb45b728f7fa02c99075acf5b
Wijayaac <wijayaac25@gmail.com>
2024-02-08 09:42:49 +0800

    Update : page structure & template some reusable section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite"
+import vitePugPlugin from "vite-plugin-pug-transformer"
 
 export default defineConfig({
 	css: {
 		devSourcemap: true
-	}
+	},
+	plugins: [vitePugPlugin()]
 })
